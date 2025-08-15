5052e3acd42b3d5321d87f0f308dd136a00b9099
Tim McEwan <timmcewandev@gmail.com>
2025-08-14 23:43:06 -0600

    Initial Commit

---
+++ GOT/GOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOTApp.swift
+//  GOT
+//
+//  Created by Tim McEwan on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
